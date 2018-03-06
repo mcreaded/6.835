8c06ec729d274f97373813cc80e8de68a685b40b
mubaarik <mmdmubaarik@gmail.com>
2018-03-06 00:26:58 -0500

    adding the long running bonus

--- miniprojects/project2/rnn.py
+++ miniprojects/project2/rnn.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, LSTM, Dense, concatenate, Bidirectional 
+from keras.layers import Input, LSTM, Dense, concatenate, Bidirectional, GRU,subtract,add,dot
 
 from normalize_frames import normalize_frames
 from load_gestures import load_gestures
@@ -66,7 +66,29 @@ def run_bid_lstm(batch_size = 12,epochs = 100,latent_dim = 16):
             validation_split=0.3,
             shuffle=True)
   return fit.history
+def run_bonus_rnn(batch_size = 12,epochs = 100,latent_dim = 16):
+  input_layer = Input(shape=(X.shape[1:]))
+  lstm = LSTM(latent_dim)(input_layer)
+  gru = GRU(latent_dim)(input_layer)
+  model1 = subtract([lstm, gru])
+  model2 = add([lstm, gru])
+  #_merged = concatenate([lstm,_lstm],axis=0)
+  dense1 = Dense(2*latent_dim, activation='tanh')(model1)
+  dense2 = Dense(2*latent_dim, activation='relu')(model2)
+  dense = add([dense1, dense2])
+  pred = Dense(len(gesture_sets), activation='softmax')(dense)
 
-#print run_bid_lstm()
+  model = Model(inputs=input_layer, outputs=pred)
+  model.compile(loss="sparse_categorical_crossentropy", optimizer='adam', metrics=["acc"])
+
+  fit = model.fit(X,
+            Y,
+            epochs=epochs,
+            batch_size=batch_size,
+            verbose=1,
+            validation_split=0.3,
+            shuffle=True)
+  return fit.history
+print run_bonus_rnn()
 
 
